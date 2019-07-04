2aab526919264e855137cf15f83506178809eeb3
Alex <alexp0205@gmail.com>
2019-07-04 22:25:27 +0530

    Adding polybar to dotfiles

---
+++ generate_links.sh
@@ -0,0 +1,7 @@
+ln -s ~/configs/xmonad-config ~/.xmonad
+ln -s ~/configs/dotfiles/Xresources ~/.Xresources
+ln -s ~/configs/dotfiles/zshrc ~/.zshrc
+ln -s ~/configs/dotfiles/zprofile ~/.zprofile
+ln -s ~/configs/dotfiles/xinitrc ~/.xinitrc
+
+ln -s ~/configs/xmonad-config/polybar/config ~/.config/polybar/config
